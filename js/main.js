b26ceee5174a15c833d12fd8fd1bf39b52482186
luca betti sorbelli <luca betti sorbelli>
2023-04-05 15:41:01 +0200

    fine

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//prompt, messaggio per l'utente
+let nomeUtente = prompt("inserisci il tuo Nome");
+let cognomeUtente = prompt("inserisci il tuo Cognome");
+colorePreferito = prompt("qual'è il tuo colore preferito?") 
+
+// variabile data
+let data = 2023;
+
+// password 
+let password = nomeUtente + cognomeUtente + colorePreferito + data;
